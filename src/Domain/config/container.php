ceedceec9646d2a5c522b38a9828ef191f46f931
Vitaliy <theyamshikov@yandex.ru>
2021-01-22 22:29:43 +0600

    upd

--- src/Domain/config/container.php
+++ src/Domain/config/container.php
@@ -23,7 +23,7 @@ return [
              */
             $driver = $_ENV['LOG_DRIVER'] ?? null;
             if ($driver == 'file') {
-                $logFileName = __DIR__ . '/../../../../../../../' . $_ENV['LOG_DIRECTORY'] . '/application.json';
+                $logFileName = __DIR__ . '/../../../../../../' . $_ENV['LOG_DIRECTORY'] . '/application.json';
                 $handler = new StreamHandler($logFileName);
                 $formatterClass = $_ENV['LOG_FORMATTER'] ?? JsonFormatter::class;
                 $formatter = $container->get($formatterClass);
