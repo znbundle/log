08aeae358092c4ac9e04230cc090d78679c38b69
Vitaliy <theyamshikov@yandex.ru>
2021-01-25 21:32:24 +0600

    upd

---
+++ src/Bundle.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ZnBundle\Log;
+
+use ZnCore\Base\Libs\App\Base\BaseBundle;
+
+class Bundle extends BaseBundle
+{
+
+    public function admin(): array
+    {
+        return [
+
+        ];
+    }
+
+    public function i18next(): array
+    {
+        return [
+
+        ];
+    }
+
+    public function migration(): array
+    {
+        return [
+            '/vendor/znbundle/log/src/Domain/Migrations',
+        ];
+    }
+
+    public function container(): array
+    {
+        return [
+            __DIR__ . '/Domain/config/container.php',
+        ];
+    }
+}
