b088718c9e4f1857c80908183165204f0a59fbf3
ZnDeveloper <zndev@ya.ru>
2022-06-20 21:09:28 +0600

    upd

--- src/Domain/Interfaces/Repositories/HistoryRepositoryInterface.php
+++ src/Domain/Interfaces/Repositories/HistoryRepositoryInterface.php
@@ -5,7 +5,7 @@ namespace ZnBundle\Log\Domain\Interfaces\Repositories;
 use ZnCore\Domain\Interfaces\GetEntityClassInterface;
 use ZnCore\Domain\Interfaces\ReadAllInterface;
 use ZnCore\Domain\Interfaces\Repository\ReadOneInterface;
-use ZnCore\Domain\Interfaces\Repository\RepositoryInterface;
+use ZnCore\Base\Libs\Repository\Interfaces\RepositoryInterface;
 
 interface HistoryRepositoryInterface extends RepositoryInterface, GetEntityClassInterface, ReadAllInterface, ReadOneInterface
 {
